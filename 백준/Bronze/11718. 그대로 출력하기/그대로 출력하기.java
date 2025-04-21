65acf16caf9a17e664e471bed5004ac89a769d05
Shuung <105972212+qmoy2510@users.noreply.github.com>
2025-04-21 16:04:24 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 112 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		
+		String str = "";
+		while((str = br.readLine()) != null) {
+			System.out.println(str);
+		}
+		
+//		bw.flush();
+		br.close();
+//		bw.close();
+	}
+}
