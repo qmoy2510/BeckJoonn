a0b237b654db0e481d3878bd2d55265caf55c96b
Shuung <105972212+qmoy2510@users.noreply.github.com>
2025-04-25 10:55:51 +0900

    [Unrated] Title: 정ㅋ벅ㅋ, Time: 92 ms, Memory: 13908 KB -BaekjoonHub

---
+++ 백준/Unrated/1237. 정ㅋ벅ㅋ/정ㅋ벅ㅋ.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Scanner;
+	
+public class Main {
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		System.out.println("문제의 정답");
+	}
+}
