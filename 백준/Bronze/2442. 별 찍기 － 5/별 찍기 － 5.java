7d689cdb782b12a7b325ba6beecfa7291c4164c8
Shuung <105972212+qmoy2510@users.noreply.github.com>
2025-05-17 23:31:15 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 116 ms, Memory: 14408 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.lang.*;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.TreeMap;
+	
+public class Main {
+	
+	static public int[] solution(int[] numlist, int n) {
+        int[] answer = {};
+        return answer;
+    }
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int num = Integer.parseInt(br.readLine());
+		for (int i = 0; i < num; i++) {
+			for (int j = 0; j < (num-i-1); j++) {
+				bw.write(" ");
+			}
+			for (int j = 0; j < 1+(i*2); j++) {
+				bw.write("*");
+			}
+			bw.write("\n");
+		}
+		
+
+		
+		bw.flush();	
+		br.close();
+		bw.close();
+	}
+}
