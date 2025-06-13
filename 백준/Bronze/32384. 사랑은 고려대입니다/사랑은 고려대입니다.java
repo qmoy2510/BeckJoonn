3a84abb223332846c950818a306b5b139af86969
Shuung <105972212+qmoy2510@users.noreply.github.com>
2025-06-13 10:33:16 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 100 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		int num = Integer.parseInt(br.readLine());
+		for (int i = 0; i < num; i++) {
+			bw.write("LoveisKoreaUniversity ");
+		}		
+		bw.write("\n");
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
