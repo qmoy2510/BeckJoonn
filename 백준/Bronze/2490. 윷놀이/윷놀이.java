d28d17a806af8d2ce170f008055288e2625f6520
Shuung <105972212+qmoy2510@users.noreply.github.com>
2025-06-29 14:15:17 +0900

    [Bronze III] Title: 윷놀이, Time: 104 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.java
+++ 백준/Bronze/2490. 윷놀이/윷놀이.java
@@ -8,27 +8,14 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		int num;
+		String str = "EABCD";
 		for (int i = 0; i < 3; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			num = 0;
 			while(st.hasMoreTokens()) {
 				num +=  Integer.parseInt(st.nextToken())==0?1:0;
 			}
-			if(num == 0) {
-				bw.write("E");
-			}
-			else if(num == 1) {
-				bw.write("A");
-			}
-			else if(num == 2) {
-				bw.write("B");
-			}
-			else if(num == 3) {
-				bw.write("C");
-			}
-			else if(num == 4) {
-				bw.write("D");
-			}
+			bw.write(str.charAt(num));
 			bw.newLine();
 		}
 		bw.flush();
