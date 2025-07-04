1723843c9b14e824cf929facca1af69651867685
Shuung <105972212+qmoy2510@users.noreply.github.com>
2025-07-04 23:45:26 +0900

    [Bronze III] Title: A+B - 6, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		int num = Integer.parseInt(br.readLine());
+		for (int i = 0; i < num; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), ",");
+			bw.write(String.valueOf(Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken())));
+			bw.newLine();
+		}
+
+		bw.flush();
+		bw.close();
+		br.close();		
+	}
+}
