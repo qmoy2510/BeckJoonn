406508279c66ba654734dcf1e6d1a2b7e2d56b20
Shuung <105972212+qmoy2510@users.noreply.github.com>
2025-07-10 18:57:58 +0900

    [Bronze V] Title: 나는 누구인가, Time: 100 ms, Memory: 14000 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+public class Main {
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		bw.write("I'm Sexy");
+		
+		bw.flush();
+		bw.close();
+		br.close();		
+	}
+}
