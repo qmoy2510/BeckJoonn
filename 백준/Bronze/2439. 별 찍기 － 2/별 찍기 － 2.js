d968b2fa4eb146cd48dd43e3648622684a07489c
Shuung <105972212+qmoy2510@users.noreply.github.com>
2025-08-04 13:38:45 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 480 ms, Memory: 13660 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const input = fs.readFileSync("/dev/stdin").toString();
+
+for(i=1; i<=input; i++){
+    for(j=0; j<input-i; j++){
+        process.stdout.write(" ");
+    }
+    for(j=0; j<i; j++){
+        process.stdout.write("*");
+    }
+    process.stdout.write("\n");
+}
