11f0500c95554cc7f1209a0b6550aa3c8a376f52
Shuung <105972212+qmoy2510@users.noreply.github.com>
2025-08-06 10:18:41 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 116 ms, Memory: 14432 KB -BaekjoonHub

---
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.math.*;import java.net.Inet4Address;
+import java.util.*;
+
+public class Main {
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		int num = Integer.parseInt(br.readLine());
+		for (int i = 0; i < num; i++) {
+			for (int j = 0; j < num-i-1; j++) {
+				bw.write(" ");
+			}
+			for (int j = 0; j < i+1; j++) {
+				bw.write("* ");
+			}
+			bw.newLine();
+		}
+		
+		bw.flush();
+		bw.close();
+		br.close();		
+	}
+}
