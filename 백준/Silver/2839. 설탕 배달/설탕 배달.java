13ba18ed1d3a2177ae9811d7e68d7089151a197b
Shuung <105972212+qmoy2510@users.noreply.github.com>
2025-08-22 08:26:22 +0900

    [Silver IV] Title: 설탕 배달, Time: 100 ms, Memory: 15136 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.math.*;	
+import java.util.*;
+
+public class Main {
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	static Integer[] arr = new Integer[100001];
+	
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		int result = fun(Integer.parseInt(br.readLine()));
+		bw.write(String.valueOf(result>100000?-1:result));
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+	public static int fun(int n) {
+//		System.out.println(n);
+		if(n==0) {
+			return 0;
+		}
+		if(n<1) {
+			return 1000000;
+		}
+		if(arr[n]!=null) {
+			return arr[n];
+		}
+
+		return arr[n]=Math.min(fun(n-3)+1,fun(n-5)+1);
+	}
+}
