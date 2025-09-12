2494ab5287a6fee45cbc6f5450c4a7387c27a2ec
Shuung <105972212+qmoy2510@users.noreply.github.com>
2025-09-12 23:50:15 +0900

    [Bronze IV] Title: 한글 2, Time: 104 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Bronze/11283. 한글 2/한글 2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.math.*;	
+import java.util.*;
+
+public class Main {
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+			
+		bw.write(String.valueOf(br.readLine().charAt(0)-'가'+1));
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
