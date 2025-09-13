5f5076e22db3ca17361d3a87f5944369315696a6
Shuung <105972212+qmoy2510@users.noreply.github.com>
2025-09-13 22:37:24 +0900

    [Bronze V] Title: Zadanie próbne, Time: 108 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/8545. Zadanie próbne/Zadanie próbne.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.math.*;	
+import java.util.*;
+
+public class Main {
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+			
+		String str = br.readLine();
+		for (int i = 0; i < str.length(); i++) {
+			bw.write(String.valueOf(str.charAt(str.length()-i-1)));
+		}
+		bw.newLine();
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
