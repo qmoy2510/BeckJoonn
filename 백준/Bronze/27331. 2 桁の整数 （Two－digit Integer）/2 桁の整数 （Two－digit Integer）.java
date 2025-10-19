19c8abb0f47981e15fc8100664daf3a5f0b62591
Shuung <105972212+qmoy2510@users.noreply.github.com>
2025-10-19 22:59:07 +0900

    [Bronze V] Title: 2 桁の整数 (Two-digit Integer), Time: 104 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Bronze/27331. 2 桁の整数 （Two－digit Integer）/2 桁の整数 （Two－digit Integer）.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.math.*;
+import java.util.*;
+
+public class Main {
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws Exception {
+		
+		bw.write(br.readLine()+br.readLine());
+		
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
